0759e3f34c13bad03bf0c4cd5cedb5c90c2bfbeb
Anthony mn <aliciasilvino@gmail.com>
2026-09-25 17:23:20 -0300

    atualização

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,82 @@
-import { questao1poo } from "./5- POO/P1.js";  
+import { questao1poo } from "./POO/P1.js";  
 document.getElementById("questao1")?.addEventListener("click", questao1poo);
 
-import { questao2poo } from "./5- POO/P2.js";  
+import { questao2poo } from "./POO/P2.js";  
 document.getElementById("questao2")?.addEventListener("click", questao2poo);
 
-import { questao3poo } from "./5- POO/P3.js";  
+import { questao3poo } from "./POO/P3.js";  
 document.getElementById("questao3")?.addEventListener("click", questao3poo);
 
-import { questao4poo } from "./5- POO/P4.js";  
+import { questao4poo } from "./POO/P4.js";  
 document.getElementById("questao4")?.addEventListener("click", questao4poo);
 
-import { questao5poo } from "./5- POO/P5.js";  
+import { questao5poo } from "./POO/P5.js";  
 document.getElementById("questao5")?.addEventListener("click", questao5poo);
 
-import { questao6poo } from "./5- POO/P6.js";  
+import { questao6poo } from "./POO/P6.js";  
 document.getElementById("questao6")?.addEventListener("click", questao6poo);
 
-import { questao7poo } from "./5- POO/P7.js";  
+import { questao7poo } from "./POO/P7.js";  
 document.getElementById("questao7")?.addEventListener("click", questao7poo);
 
-import { questao8poo } from "./5- POO/P8.js";  
+import { questao8poo } from "./POO/P8.js";  
 document.getElementById("questao8")?.addEventListener("click", questao8poo);
 
-import { questao9poo } from "./5- POO/P9.js";  
+import { questao9poo } from "./POO/P9.js";  
 document.getElementById("questao9")?.addEventListener("click", questao9poo);
 
-import { questao10poo } from "./5- POO/P10.js";  
+import { questao10poo } from "./POO/P10.js";  
 document.getElementById("questao10")?.addEventListener("click", questao10poo);
 
-import { questao11poo } from "./5- POO/P11.js";  
+import { questao11poo } from "./POO/P11.js";  
 document.getElementById("questao11")?.addEventListener("click", questao11poo);
 
-import { questao12poo } from "./5- POO/P12.js";  
+import { questao12poo } from "./POO/P12.js";  
 document.getElementById("questao12")?.addEventListener("click", questao12poo);
 
-import { questao13poo } from "./5- POO/P13.js";  
+import { questao13poo } from "./POO/P13.js";  
 document.getElementById("questao13")?.addEventListener("click", questao13poo);
 
-import { questao14poo } from "./5- POO/P14.js";  
+import { questao14poo } from "./POO/P14.js";  
 document.getElementById("questao14")?.addEventListener("click", questao14poo);
 
-import { questao15poo } from "./5- POO/P15.js";  
+import { questao15poo } from "./POO/P15.js";  
 document.getElementById("questao15")?.addEventListener("click", questao15poo);
 
-import { questao16poo } from "./5- POO/P16.js";  
+import { questao16poo } from "./POO/P16.js";  
 document.getElementById("questao16")?.addEventListener("click", questao16poo);
 
-import { questao17poo } from "./5- POO/P17.js";  
+import { questao17poo } from "./POO/P17.js";  
 document.getElementById("questao17")?.addEventListener("click", questao17poo);
 
-import { questao18poo } from "./5- POO/P18.js";  
+import { questao18poo } from "./POO/P18.js";  
 document.getElementById("questao18")?.addEventListener("click", questao18poo);
 
-import { questao19poo } from "./5- POO/P19.js";  
+import { questao19poo } from "./POO/P19.js";  
 document.getElementById("questao19")?.addEventListener("click", questao19poo);
 
-import { questao20poo } from "./5- POO/P20.js";  
+import { questao20poo } from "./POO/P20.js";  
 document.getElementById("questao20")?.addEventListener("click", questao20poo);
 
-import { questao23poo } from "./5- POO/P23.js";  
+import { questao23poo } from "./POO/P23.js";  
 document.getElementById("questao23")?.addEventListener("click", questao23poo);
 
-import { questao30poo } from "./5- POO/P30.js";  
+import { questao30poo } from "./POO/P30.js";  
 document.getElementById("questao30")?.addEventListener("click", questao30poo);
 
-import { questao36poo } from "./5- POO/P36.js";
+import { questao36poo } from "./POO/P36.js";
 document.getElementById("questao36")?.addEventListener("click", questao36poo);
 
-import { questao39poo } from "./5- POO/P39.js";
+import { questao39poo } from "./POO/P39.js";
 document.getElementById("questao39")?.addEventListener("click", questao39poo);
 
-import { questao42poo } from "./5- POO/P42.js";
+import { questao42poo } from "./POO/P42.js";
 document.getElementById("questao42")?.addEventListener("click", questao42poo);
 
-import { questao47poo } from "./5- POO/P47.js";
+import { questao47poo } from "./POO/P47.js";
 document.getElementById("questao47")?.addEventListener("click", questao47poo);
 
-import { questao49poo } from "./5- POO/P49.js";
+import { questao49poo } from "./POO/P49.js";
 document.getElementById("questao49")?.addEventListener("click", questao49poo);
 
 
